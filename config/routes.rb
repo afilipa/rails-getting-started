828f158d41ac224bef54e131574bcf2c7676d058
Ana Teixeira <afilipa_teixeira@hotmail.com>
2021-11-10 15:28:15 +0000

    crud comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # get "/articles/:id", to: "articles#show"
   # |
   # v
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
